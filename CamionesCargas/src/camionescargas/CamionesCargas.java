56e1ff429157783a6bfaf097f3bc3652330928d3
tsagames99@hotmail.com <>
2018-06-11 10:49:33 -0300

    LISTO

--- CamionesCargas/src/camionescargas/CamionesCargas.java
+++ CamionesCargas/src/camionescargas/CamionesCargas.java
@@ -5,6 +5,8 @@
  */
 package camionescargas;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Alumno
@@ -15,9 +17,26 @@ public class CamionesCargas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Camion camion1=new Camion();
-        Sector sect1=new Sector();
+       
+       
         Producto produ1=new Producto('f',44,33);
+        Producto produ2=new Producto('f',44,33);
+        Producto produ3=new Producto('f',44,33);
+        Producto produ4=new Producto('f',44,33);
+        Producto produ5=new Producto('f',44,33);
+        // carga
+        
+        ArrayList <Producto> arreglo_cargas = new ArrayList ();
+        arreglo_cargas.add(produ1);
+        arreglo_cargas.add(produ2);
+        arreglo_cargas.add(produ3);
+        arreglo_cargas.add(produ4);
+        arreglo_cargas.add(produ5);
+        // creacion de secto
+        Sector sector1 = new Sector (400,350, arreglo_cargas);
+        // LISTO
+        
+        
     }
     
 }
